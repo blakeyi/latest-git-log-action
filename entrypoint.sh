e911c3eda609190350363166559e40dae8f2d4e4
blakeyi <1152420971@qq.com>
2023-03-28 12:31:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-
-if [[ $1 != '' ]]
-then
-echo "tag is $1"
-log=$(git log $1 -1 --pretty=format:'- %cd %an %n  %s%n %n' --date=format:'%Y-%m-%d %H:%M:%S')
+if [[ $1 != '' ]]; then
+    if [[ $1 == 'log' ]]; then
+        if [[ $2 != '' ]]; then
+            echo "commitID is $2"
+            log=$(git log $2)
+        else
+            echo "get latest log"
+            log=$(git log -1)
+        fi
+    elif [[ $1 == 'tag' ]]; then
+        if [[ $2 != '' ]]; then
+            echo "tag is $2"
+            log=$(git log $tagName)
+        else
+            tagName=$(git describe --abbrev=0 --tags)
+            echo "tag is $tagName"
+            log=$(git log $tagName)
+        fi
+    fi
 
 else
-tagName=$(git describe --abbrev=0 --tags)
-echo "tag is $tagName"
-log=$(git log $tagName.. --pretty=format:'- %cd %an %n  %s%n %n' --date=format:'%Y-%m-%d %H:%M:%S')
+    echo "get latest log"
+    log=$(git log -1)
 
 fi
 
